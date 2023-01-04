cc7ff88afd20e38cafeb81edfcbdba8360bb9106
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-01-03 23:50:52 -0300

    first commit

---
+++ src/components/OurChoices.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function OurChoices() {
+  return (
+    <div>OurChoices</div>
+  )
+}
+
+export default OurChoices
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import OurChoices from "../components/OurChoices";
+import Popular from "../components/Popular";
+
+function Home() {
+  return (
+    <div>
+        <OurChoices/>
+        <Popular/>
+    </div>
+  )
+}
+
+export default Home
