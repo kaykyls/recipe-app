253e2ad60725179de9bf7c9cb0691f6cea11cedf
kaykyls <20211bsifsa0025@ifba.edu.br>
2023-01-13 22:19:54 -0300

    started search

---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {FaSearch} from 'react-icons/fa'
+import "./search.css"
+
+const Search = () => {
+  return (
+    <form className="form-container" action="">
+        <input type="text" />
+    </form>
+  )
+}
+
+export default Search
